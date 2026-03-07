21514ed0459917d694578b7bdc7df337f482751e
StephenJD <sjdolley@gmail.com>
2026-03-07 16:09:03 +0000

    Add content editor generator to dynamic imports

--- netlify/functions/dynamic_generators/generator_imports.js
+++ netlify/functions/dynamic_generators/generator_imports.js
@@ -1,7 +1,9 @@
 import generate_vist_stats from "./generate_vist_stats.js";
 import generate_document_review from "./generate_document_review.js";
+import generate_content_editor from "./generate_content_editor.js";
 
 export const dynamicRuntimes = {
   generate_vist_stats,
-  generate_document_review
+  generate_document_review,
+  generate_content_editor
 };
