5f3c17449b3a92f9f1adba71e4443f140b7cb091
StephenJD <sjdolley@gmail.com>
2026-03-08 22:24:01 +0000

    Delete static/js/webeditor/log.js

--- static/js/webeditor/log.js
+++
@@ -1,9 +0,0 @@
-// \static\js\webeditor\log.js
-window.log = function(msg) {
-    const logDiv = document.getElementById("logDiv");
-    if(logDiv) {
-        logDiv.textContent += msg + "\n";
-        logDiv.scrollTop = logDiv.scrollHeight;
-    }
-    console.log(msg);
-}
