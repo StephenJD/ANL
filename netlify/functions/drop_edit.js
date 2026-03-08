94bc6a463bf366c981cd5b28d925fcb041e763a0
StephenJD <sjdolley@gmail.com>
2026-03-08 17:56:45 +0000

    Add drop_edit.js function handler

---
+++ netlify/functions/drop_edit.js
@@ -0,0 +1,33 @@
+// netlify/functions/drop_edit.js
+import fs from "fs";
+import path from "path";
+
+export default async function handler(event){
+
+try{
+
+const {file}=JSON.parse(event.body);
+
+const tmpPath=path.join(process.cwd(),"tmp",file);
+
+if(fs.existsSync(tmpPath)){
+fs.unlinkSync(tmpPath);
+}
+
+return new Response(JSON.stringify({ok:true}),{
+status:200,
+headers:{"Content-Type":"application/json"}
+});
+
+}catch(err){
+
+console.error("[drop_edit]",err);
+
+return new Response(JSON.stringify({error:"drop failed"}),{
+status:500,
+headers:{"Content-Type":"application/json"}
+});
+
+}
+
+}
