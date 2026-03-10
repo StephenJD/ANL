d9b7552b0c6448b083cc532a9635f21508365b61
StephenJD <sjdolley@gmail.com>
2026-03-10 15:38:50 +0000

    Refactor qualification function logic
    Refactor qualification function to handle parentType check for 'collated_page'. Removed redundant checks for children types.

--- netlify/functions/webeditor/qualification.js
+++ netlify/functions/webeditor/qualification.js
@@ -2,20 +2,19 @@
 export function qualification (type, parentType = null) {
   if (!type) return "Unknown";
 
-  // If parent is collated_page, children are Sections
-  if (parentType === "collated_page" && type !== "home" && type !== "collated_page") {
-    return `Section:`;
-  }
-
   switch(type){
     case "dynamic":
     case "login":
     case "form":
     case "document":
+      if (parentType === "collated_page") {
+          return `Section:`;
+      }
       return `Content:`;
     case "home":
     case "collated_page":
-      return `Collated:`;
+      return `Collated:`
+    case "document-folder":
     case "see_also":
       return `Navigation:`;
     default:
